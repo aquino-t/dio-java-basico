5fc1d4a99d3c8986cd5c06c9ae337ddba698998d
Thiago <aquino.t@tutanota.com>
2023-09-28 19:47:13 -0300

    final commit

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite o número da agência!");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da conta!");
+        String conta = scanner.next();
+
+        System.out.println("Por favor, digite o seu nome!");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite o seu saldo!");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "+ conta + " e seu saldo [" + saldo + "] já está disponível para saque.");
     }
+
 }
